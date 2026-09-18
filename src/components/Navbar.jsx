755d33e91a7e466ea0905e1e53c96608cae7e507
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2026-09-18 16:58:31 +0600

    Refactor Navbar component to include application title and navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
+import { NavLink } from "react-router";
+
 const Navbar = () => {
-    return (
-        <nav>
-            <h1>Welcome to the Navbar</h1>
-        </nav>
-    )
+  return (
+    <nav className=" bg-gray-800 text-white p-4 ">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold tracking-wide">
+          Movie<span className="text-yellow-400">Expo</span>
+        </h1>
+        <ul className="flex gap-6">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `transition duration-200 ${isActive ? "text-yellow-400 font-semibold" : "text-white hover:underline"}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/movies"
+              className={({ isActive }) =>
+                `transition duration-200 ${isActive ? "text-yellow-400 font-semibold" : "text-white hover:underline"}`
+              }
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
